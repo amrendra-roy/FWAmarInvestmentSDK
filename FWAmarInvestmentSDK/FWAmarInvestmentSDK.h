9843b74a50d3ea84e2f1bcfe8347bf0cb429ec09
Amrendra <amar.roy17@gmail.com>
2023-01-08 19:59:10 +0530

    Initial Commit

---
+++ FWAmarInvestmentSDK/FWAmarInvestmentSDK.h
@@ -0,0 +1,18 @@
+//
+//  FWAmarInvestmentSDK.h
+//  FWAmarInvestmentSDK
+//
+//  Created by Amrendra on 08/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FWAmarInvestmentSDK.
+FOUNDATION_EXPORT double FWAmarInvestmentSDKVersionNumber;
+
+//! Project version string for FWAmarInvestmentSDK.
+FOUNDATION_EXPORT const unsigned char FWAmarInvestmentSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FWAmarInvestmentSDK/PublicHeader.h>
+
+
